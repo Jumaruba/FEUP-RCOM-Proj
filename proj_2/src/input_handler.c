466ed86a7a3101c14aa138ce1b61fe0ec2aaa3ef
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2020-12-02 14:45:38 +0000

    spliting, but not adding in the struct

--- proj_2/src/input_handler.c
+++ proj_2/src/input_handler.c
@@ -21,35 +21,49 @@ int input_handler(int argc, char **argv, HostRequestData* data){
     // Store website info inside data struct.  
     parse_input(remain_url, data);  
     
-
 	return 0; 
  
 } 
 void parse_input(char* remain_url, HostRequestData* data){  
-    char parameters[4][MAX_STRING_LEN]; 
+    char parameters[6][MAX_STRING_LEN]; 
     split_input(remain_url, parameters);  
 
 }  
 
 void split_input(char* remain_url, char parameters[][MAX_STRING_LEN]){
-    char delim[] = ":@/";    
+    char delim[] = ":@";    
     int counter = 0;   
 
     char * ptr = strtok(remain_url, delim);  
 
-    while(ptr != NULL && counter < 4){
+
+    while(ptr != NULL && counter < 3){
         memcpy(parameters[counter], ptr, strlen(ptr));          // Store parameter.  
         PRINTF("url parameter: %s\n", ptr); 
         counter ++;                          
         ptr = strtok(NULL, delim);                              // Split.   
-    }   
+    }            
+    
+    // Get the host.
+    int first_bar_pos = strcspn(parameters[2], "/"); 
+    memcpy(parameters[3], &parameters[2], first_bar_pos); 
+    parameters[3][first_bar_pos] = '\0'; 
 
-    if (counter < 4){
+
+
+
+    printf("%s\n", parameters[2]);
+    printf("%s\n", parameters[3]); 
+
+
+    memcpy(parameters[4], &parameters[2][first_bar_pos+1], MAX_STRING_LEN); 
+    printf("%s\n", parameters[4]);
+    printf("%d\n", first_bar_pos);
+    if (counter < 3){
         PRINT_ERR("Wrong url format ftp://[<user>:<password>@]<host>/<url-path>\n");
         exit(-1); 
     }
 }
-
 
 
 	
