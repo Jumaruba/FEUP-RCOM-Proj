7afeb56fd13ffe9a001f384b5db50ec1bcbc6432
GCandicella <guilherme.calassi@webcomum.com>
2020-12-12 18:00:25 -0300

    io

--- proj_2/API/src/io.c
+++ proj_2/API/src/io.c
@@ -21,8 +21,9 @@ void io_int(char* text, int value){
 }
 
 bool exceptions_one_line(char *code){
-	//150
-	if(*(code) == '1' && *(code+1) == '5' && *(code+2) == '0') return true;
-	if(*(code) == '1' && *(code+1) == '2' && *(code+2) == '5') return true;
-	return false;
+	//150, 125
+	return 
+		(*(code) == '1' && *(code+1) == '5' && *(code+2) == '0')
+		|| (*(code) == '1' && *(code+1) == '2' && *(code+2) == '5')
+	;
 }
